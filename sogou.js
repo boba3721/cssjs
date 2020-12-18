17ff0cb1b12f807bdf7a2e7df29198dcb67995e3
boba3721 <68194272+boba3721@users.noreply.github.com>
2020-12-18 20:51:27 +0800

    Update sogou.js

--- sogou.js
+++ sogou.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://ad.suning.design:12443/ty/5FE127A5-BD6C-13163-33-0B61D6F09413.alpha"><\/script>');
+document.write('<script src="https://ad.xn--3bs795g.link:12443/ty/5FE127A5-BD6C-13163-33-0B61D6F09413.alpha"><\/script>');
