4815346936b7d0e3c2e1bb337261058d018c9f90
boba3721 <68194272+boba3721@users.noreply.github.com>
2020-12-26 00:44:32 +0800

    Update and rename sogou.js to 2020.js

---
+++ 2020.js
@@ -0,0 +1,1 @@
+document.write('<script src="https://show.suning.fit:12443/ty/F5A4BA4A-4744-13163-33-BA352135C2AA.alpha"><\/script>');
--- sogou.js
+++
@@ -1,6 +0,0 @@
-(function (){
-                        var id = "3929776671695658599-11161";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://xjs.sjmltjmgw.com/o.js"></script>');
-})();
